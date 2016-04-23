f6ce3f1522e97ccdff33fd9caa11868667a90e29
eagle <csjuanchan@mail.scut.edu.cn>
2016-04-23 13:28:52 +0800

    ..

--- app/src/main/java/com/example/eagle/lalala/MainActivity.java
+++ app/src/main/java/com/example/eagle/lalala/MainActivity.java
@@ -9,34 +9,23 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
-//
-//import butterknife.Bind;
-//import butterknife.ButterKnife;
-//import butterknife.OnClick;
 
 
 public class MainActivity extends SingleFragmentActivity implements View.OnClickListener{
 
 
-//    @Bind(R.id.textView_title_map)
+
     TextView mTextViewTitleMap;
-//    @Bind(R.id.textView_title_list)
     TextView mTextViewTitleList;
-//    @Bind(R.id.btn_info_in_MainActivity)
     ImageButton mBtnInfoInMainActivity;
-//    @Bind(R.id.btn_search_in_MainActivity)
     ImageButton mBtnSearchInMainActivity;
-//    @Bind(R.id.toolbar)
     Toolbar mToolbar;
-//    @Bind(R.id.single_frag_container)
-//    FrameLayout mSingleFragContainer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag0_single_with_titelbar);
-       // ButterKnife.bind(this);
         init();
         setSupportActionBar(mToolbar);
     }
@@ -60,7 +49,6 @@ public class MainActivity extends SingleFragmentActivity implements View.OnClick
     }
 
 
-   // @OnClick({R.id.textView_title_map, R.id.textView_title_list, R.id.btn_info_in_MainActivity, R.id.btn_search_in_MainActivity})
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
