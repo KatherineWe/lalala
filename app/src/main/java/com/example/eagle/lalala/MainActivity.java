89cd130521c715d3b0d5753c0c48ae6553ba825a
eagle <csjuanchan@mail.scut.edu.cn>
2016-04-23 16:11:33 +0800

    ...

--- app/src/main/java/com/example/eagle/lalala/MainActivity.java
+++ app/src/main/java/com/example/eagle/lalala/MainActivity.java
@@ -2,45 +2,123 @@ package com.example.eagle.lalala;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.eagle.lalala.PictureWork.TakePicture;
+import com.example.neilhy.floatingbutton_library.FloatingActionButton;
+import com.example.neilhy.floatingbutton_library.FloatingActionMenu;
 
-public class MainActivity extends SingleFragmentActivity implements View.OnClickListener{
+import java.io.File;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
+public class MainActivity extends SingleFragmentActivity implements View.OnClickListener {
 
+
+    @Bind(R.id.textView_title_map)
     TextView mTextViewTitleMap;
+    @Bind(R.id.textView_title_list)
     TextView mTextViewTitleList;
+    @Bind(R.id.btn_info_in_MainActivity)
     ImageButton mBtnInfoInMainActivity;
+    @Bind(R.id.btn_search_in_MainActivity)
     ImageButton mBtnSearchInMainActivity;
+    @Bind(R.id.toolbar)
     Toolbar mToolbar;
+    @Bind(R.id.single_frag_container)
+    FrameLayout mSingleFragContainer;
+    @Bind(R.id.fab_add)
+    FloatingActionButton fab_add;
+    @Bind(R.id.fab_camera)
+    FloatingActionButton fab_camera;
+    @Bind(R.id.menu_button)
+    FloatingActionMenu menuButton;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @Bind(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    private Handler mUiHandler=new Handler();
+    private File imageFile;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frag0_single_with_titelbar);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         init();
-        setSupportActionBar(mToolbar);
+
     }
 
 
     private void init() {
-        mTextViewTitleMap = (TextView)findViewById(R.id.textView_title_map);
-        mBtnSearchInMainActivity = (ImageButton)findViewById(R.id.btn_search_in_MainActivity);
-        mBtnInfoInMainActivity = (ImageButton)findViewById(R.id.btn_info_in_MainActivity);
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        mTextViewTitleList = (TextView)findViewById(R.id.textView_title_map);
-        mTextViewTitleList.setOnClickListener(this);
-        mTextViewTitleMap.setOnClickListener(this);
-        mBtnInfoInMainActivity.setOnClickListener(this);
-        mBtnSearchInMainActivity.setOnClickListener(this);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+
+                switch (item.getItemId()) {
+                    case R.id.menu_message:
+
+                        break;
+                    case R.id.menu_favorite:
+
+                        break;
+                    case R.id.menu_likes:
+
+                        break;
+                    case R.id.menu_posted:
+
+                        break;
+                    case R.id.menu_setting:
+
+                        break;
+                    default:
+
+                        break;
+                }
+
+
+                return true;
+            }
+        });
+        setSupportActionBar(mToolbar);
+        setSupportActionBar(mToolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        fab_add.setOnClickListener(this);
+        fab_camera.setOnClickListener(this);
+
+        menuButton.setClosedOnTouchOutside(true);//点击外部可以关闭选项
+        menuButton.hideMenuButton(false);
+
+        mUiHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                menuButton.showMenuButton(true);
+            }
+        },500);//让这个按钮500毫秒之后显示出来
+
+        TakePicture.createCustomAnimation(menuButton);//设置点击按钮后的动画，星星变叉
+
     }
 
     @Override
@@ -49,17 +127,13 @@ public class MainActivity extends SingleFragmentActivity implements View.OnClick
     }
 
 
-    @Override
+    @OnClick({R.id.textView_title_map, R.id.textView_title_list, R.id.btn_info_in_MainActivity, R.id.btn_search_in_MainActivity})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.textView_title_map:
-                mTextViewTitleMap.setTextColor(0xffffffff);
-                mTextViewTitleList.setTextColor(0xb0ffffff);
                 changeFrag(new MapFragment());
                 break;
             case R.id.textView_title_list:
-                mTextViewTitleList.setTextColor(0xffffffff);
-                mTextViewTitleMap.setTextColor(0xb0ffffff);
                 changeFrag(new SharedFragment());
                 break;
             case R.id.btn_info_in_MainActivity:
